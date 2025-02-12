80884af0d65c52915b33d045505952f7021e89c8
Christian Bartram <cbartram3@gmail.com>
2025-02-12 18:42:29 -0500

    fix: Change cognito installed backups to map structure

--- server/delete_server_handler.go
+++ server/delete_server_handler.go
@@ -62,7 +62,7 @@ func (d *DeleteServerHandler) HandleRequest(c *gin.Context, kubeService service.
 		},
 		{
 			Name:  aws.String("custom:installed_backups"),
-			Value: aws.String("[]"),
+			Value: aws.String("{}"),
 		},
 	})
 
